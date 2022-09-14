ec70d3ae2a41f8442ad1c5bf30a12f6dd99c1eab
Lily <2542101862@qq.com>
2022-09-14 21:40:32 +0800

    新增路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
+import Notebooks from '@/components/NotebookList'
+import NoteDetail from '@/components/NoteDetail'
+import TrashDetail from '@/components/TrashDetail'
 
 Vue.use(Router)
 
@@ -13,9 +16,20 @@ export default new Router({
       component: HelloWorld
     },
     {
-      path: '/Login',
-      name: 'Login',
+      path: '/login',
       component: Login
     },
+    {
+      path: '/notebooks',
+      component: Notebooks
+    },
+    {
+      path: '/note/:noteId',
+      component: NoteDetail
+    },
+    {
+      path: '/trash/:noteId',
+      component: TrashDetail
+    }
   ]
 })
